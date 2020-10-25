6a4d081e6cc143a059e986bcfb36a03c93e3c5df
Malina228 <71219885+Malina228@users.noreply.github.com>
2020-10-25 23:18:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,23 @@
-module.exports = function check(str, bracketsConfig) {
-  const strArr = str.split('');
-  let temp = [];
-  for (let i = 0; i < strArr.length; i += 1) {
-    const bracket = strArr[i];
+module.exports = function check (str, bracketsConfig) {
+  const strArray = str.split('')
+  const qwerty = Object.fromEntries(bracketsConfig)
+  const res = []
 
-    for (let j = 0; j < bracketsConfig.length; j += 1) {
-      if (bracket === bracketsConfig[j][0] && bracketsConfig[j][0] === bracketsConfig[j][1]) {
-            if (bracket === temp.slice(-1)[0]) {
-              temp = temp.slice(0, -1);
-            } else {
-              temp = [...temp, bracket];
-            }
+  for (let i of strArray) 
+  {
+      if (res[0] === i)
+      {
+          res.shift()
+      } 
+      else if (qwerty[i]) 
+      {
+          res.unshift(qwerty[i])
+      } 
+      else 
+      {
+          return false
       }
-
-      if (bracket === bracketsConfig[j][0]) {
-        temp = [...temp, bracket];
-      }
-
-      if (bracket === bracketsConfig[j][1] && temp.slice(-1)[0] === bracketsConfig[j][0] && temp.length > 0) {
-        temp = temp.slice(0, -1);
-      } else if (bracket === bracketsConfig[j][1]) {
-          if (bracketsConfig[j][1] === bracketsConfig[j][0]) {
-            continue;
-          } else {
-            return false;
-          }
-      }
-    }
   }
 
-  return temp.length === 0;
+  return res.length === 0
 }
